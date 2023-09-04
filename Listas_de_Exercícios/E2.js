c457352de72d38a3d260a62cd5cbf7ae8bd6689d
Marcos Wiendl <127610129+mwndl@users.noreply.github.com>
2023-09-04 20:44:59 -0300

    Add files via upload

---
+++ Listas_de_Exercícios/E2.js
@@ -0,0 +1,14 @@
+// Solicita a nota 1
+let nota1 = prompt("Digite a nota 1: ");
+
+// Solicita a nota 2
+let nota2 = prompt("Digite a nota 2: ");
+
+// Solicita a nota 3
+let nota3 = prompt("Digite a nota 3: ");
+
+// Calcula a média ponderada
+let media = (nota1 * 2 + nota2 * 3 + nota3 * 5) / (2 + 3 + 5);
+
+// Imprime a média
+alert("A média ponderada é de " + media);
